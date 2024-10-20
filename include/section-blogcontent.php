8267d01ffa2e2b354c25796f368f13318148d536
Mausam Pariyar <mausam.2020@lagrandee.edu.np>
2024-10-20 20:26:27 +0545

    Fn added to dispplay the date and time of post creation

--- include/section-blogcontent.php
+++ include/section-blogcontent.php
@@ -1,10 +1,14 @@
 <?php if(have_posts()):while(have_posts()):the_post();?>
-    
+
+<!-- Wordpress function to display the exact date and time of the post creation. -->
+  <p><?php echo get_the_date('d/m/Y h:i:s');?></p> 
+  <!-- Wordpress fn to display the content in the respective page. -->
   <?php the_content();?>
 
+  <!-- WP fn to display the author meta details -->
   <?php 
   $fname = get_the_author_meta('first_name');
   $lname = get_the_author_meta('last_name');
-  echo 'Source:'.' '.$fname .' '.$lname;
+  echo 'Posted by'.' '.$fname .' '.$lname;
   ?>
 <?php endwhile; else: endif;?>

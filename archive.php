84d513376434cdade72c8e59a624898baef2e789
Mausam Pariyar <mausam.2020@lagrandee.edu.np>
2024-10-20 20:04:27 +0545

    Pagination style 2 added in archive.php

--- archive.php
+++ archive.php
@@ -5,10 +5,20 @@
 
     <?php get_template_part('include/section','archive');?>
     <!-- 1st way for pagination in WP -->
-    <?php previous_posts_link();?>
-    <?php next_posts_link();?>
+    <!-- <?php previous_posts_link();?>
+    <?php next_posts_link();?> -->
 
     <!-- 2nd way for pagination in WP-->
+     <?php
+        global $wp_query;
+        $big = 999999999; // need an unlikely integer
+        echo paginate_links( array(
+            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+            'format' => '?paged=%#%',
+            'current' => max( 1, get_query_var('paged') ),
+            'total' => $wp_query->max_num_pages
+        ) );
+     ?>
 </div>
 </section>
 <?php get_footer();?>

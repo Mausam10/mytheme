6a08884f962a2dc2f9aa689e0c3591d60df0ac9c
Mausam Pariyar <mausam.2020@lagrandee.edu.np>
2024-10-24 00:18:03 +0545

    Custom Post Types added

--- include/section-cars.php
+++ include/section-cars.php
@@ -1,16 +1,9 @@
 <?php if(have_posts()):while(have_posts()):the_post();?>
 
-<!-- Wordpress function to display the exact date and time of the post creation. -->
-  <p><?php echo get_the_date('F j, Y  h:i:s');?></p> 
+
   <!-- Wordpress fn to display the content in the respective page. -->
   <?php the_content();?>
 
-  <!-- WP fn to display the author meta details -->
-  <?php 
-  $fname = get_the_author_meta('first_name');
-  $lname = get_the_author_meta('last_name');
-  echo 'Posted by'.' '.$fname .' '.$lname;
-  ?>
   <br>
   <?php
   $tags = get_the_tags();
@@ -31,7 +24,8 @@
 
     <a href="<?php echo get_category_link($cat->term_id);?>"><?php echo $cat->name;?></a>
     <?php endforeach;?>
-    
+    <!-- Wordpress function to display the exact date and time of the post creation. -->
+  <p><?php echo get_the_date('F j, Y  h:i:s');?></p> 
     <!-- WP fn to display the comment in the page -->
     <!-- <?php //comments_template();?> -->
 <?php endwhile; else: endif;?>
